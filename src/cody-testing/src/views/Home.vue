0f0594d363fa918164f18affc679380cdd9b452c
JoeRapso <joe.rapso@gmail.com>
2021-04-04 00:39:26 +0300

    class changes with function call

--- src/cody-testing/src/views/Home.vue
+++ src/cody-testing/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1 class="page-header">Page Header</h1>
-    <AlertEffect icon="Warning">
+    <AlertEffect icon="Success">
       <p class="text-sm"><strong>Info:</strong> <a href="#0" class="color-inherit">Learn more</a></p>
     </AlertEffect> 
   </div>
